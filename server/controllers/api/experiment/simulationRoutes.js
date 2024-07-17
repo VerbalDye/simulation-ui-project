333b4cadb67184883add1b185e871dbb1b18c1ae
Dustin Huntsberger <dustin.huntsberger@engusasecure.com>
2024-07-17 14:16:58 -0400

    fixed simulation routes

--- server/controllers/api/experiment/simulationRoutes.js
+++ server/controllers/api/experiment/simulationRoutes.js
@@ -82,29 +82,29 @@ router.post('/start/:id', (req, res) => {
             }
         ]
     }
-    Promise.allSettled([
-        new Promise(resolve => {
-            ExperimentInfo.create({
-                experiment_id: req.params.id
-            })
-                .then(dbExperimentInfoData => resolve())
-                .catch(err => {
-                    console.log(err);
-                    res.status(400).json(err);
-                });
-        }),
-        new Promise(resolve => {
-            // CurrentlyRunning.create({
-            //     experiment_id: req.params.id
-            // })
-            //     .then(dbCurrentlyRunningData => resolve())
-            //     .catch(err => {
-            //         console.log(err);
-            //         res.status(400).json(err);
-            //     });
-        })
-    ])
-        .then(dbPromiseData => {
+    // Promise.allSettled([
+    //     new Promise(resolve => {
+    //         ExperimentInfo.create({
+    //             experiment_id: req.params.id
+    //         })
+    //             .then(dbExperimentInfoData => resolve())
+    //             .catch(err => {
+    //                 console.log(err);
+    //                 res.status(400).json(err);
+    //             });
+    //     }),
+    //     // new Promise(resolve => {
+    //         // CurrentlyRunning.create({
+    //         //     experiment_id: req.params.id
+    //         // })
+    //         //     .then(dbCurrentlyRunningData => resolve())
+    //         //     .catch(err => {
+    //         //         console.log(err);
+    //         //         res.status(400).json(err);
+    //         //     });
+    //     // })
+    // ])
+        // .then(dbPromiseData => {
             fetch(url, {
                 method: "POST", // *GET, POST, PUT, DELETE, etc.
                 mode: "cors", // no-cors, *cors, same-origin
@@ -120,7 +120,7 @@ router.post('/start/:id', (req, res) => {
             }).then(result => {
                 res.status(200).json(result);
             })
-        })
+        // })
 })
 
 module.exports = router;

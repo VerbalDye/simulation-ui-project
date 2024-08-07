274a1dcb57f33147807c62b06558330717d4571c
Dustin Huntsberger <dustin.huntsberger@engusasecure.com>
2024-08-07 13:17:45 -0400

    add admin processing times page

--- client/src/views/ProcessTimeDefaults.vue
+++ client/src/views/ProcessTimeDefaults.vue
@@ -5,6 +5,50 @@
         <Sidebar />
         <div class="content">
             <h1>Process Time Defaults</h1>
+            <div>
+                <!-- <VueMultiselect v-model="selectedAssets"
+                    :options="this.formattedAssets" :multiple="true" :close-on-select="false"
+                    placeholder="Select at least one model"
+                    @update:model-value="handleModelSelectChange(selectedAssets[0].asset_id)" :preselect-first="true">
+                    <template slot="selection" slot-scope="{ values, search, isOpen }"><span class="multiselect__single"
+                            v-show="!isOpen">
+                            options
+                            selected</span></template>
+                </VueMultiselect>
+                <VueMultiselect v-model="this.processTimeSettings.selectedModels[selectedAssets[0].asset_id]"
+                    :options="this.processTimeSettings.modelData" :multiple="true" :close-on-select="false"
+                    placeholder="Select at least one model"
+                    @update:model-value="handleModelSelectChange(selectedAssets[0].asset_id)" :preselect-first="true">
+                    <template slot="selection" slot-scope="{ values, search, isOpen }"><span class="multiselect__single"
+                            v-show="!isOpen">
+                            options
+                            selected</span></template>
+                </VueMultiselect>
+                <table class="grid-less">
+                    <tr>
+                        <th><i class="bi bi-hash"></i> Number of Samples*</th>
+                        <td>
+                            <input type="number"
+                                :value="Object.keys(processTimeSettings.elements[selectedAssets[0].asset_id].values).length"
+                                class="small-number-input" :name="'samples-' + selectedAssets[0].asset_id"
+                                @input="handleNumberOfSamplesChange(selectedAssets[0].asset_id, processTimeSettings.selectedModels[selectedAssets[0].asset_id][0], $event)">
+                        </td>
+                    </tr>
+                    <tr>
+                        <th><i class="bi bi-clock-fill"></i> Enter Processing
+                            Times
+                            (minutes)*</th>
+                        <td>
+                            <div class="flex-left">
+                                <input
+                                    v-for="(value, key) in this.processTimeSettings.elements[selectedAssets[0].asset_id].values"
+                                    class="small-number-input" type="number" :value="value" :name="'times-' + key"
+                                    @input="handleProcessTimeDataChange(key, $event)">
+                            </div>
+                        </td>
+                    </tr>
+                </table> -->
+            </div>
         </div>
     </div>
 </template>
@@ -13,6 +57,7 @@
 import Header from '@/components/Header.vue';
 import titleMixin from '../mixins/titleMixin';
 import Sidebar from '@/components/Sidebar.vue';
+import VueMultiselect from 'vue-multiselect';
 import auth from '../utils/auth';
 import dataRequest from '@/utils/dataRequest';
 import dayjs from 'dayjs';
@@ -20,16 +65,21 @@ export default {
     data() {
         return {
             assetData: {},
+            formattedAssets: {},
             experimentID: 2,
+            selectedAssets: [],
+            processTimeSettings: {
+                selectedModels: []
+            }
         }
     },
     mixins: [titleMixin],
     title: 'Process Time Default',
-    components: { Sidebar, Header },
+    components: { Sidebar, Header, VueMultiselect },
     methods: {
         async getAssetData() {
             let data = await dataRequest("/api/experiment/asset/" + this.experimentID, "GET");
-            // console.log(data);
+            console.log(data);
             this.assetData = data.map(e => e.asset);
         },
         async getProcessTimeData() {
@@ -42,6 +92,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>

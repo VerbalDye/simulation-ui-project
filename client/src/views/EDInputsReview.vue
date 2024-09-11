60d0a82ac4abecb6ae6e2ee73d5545ea9c183d36
Dustin Huntsberger <dustin.huntsberger@engusasecure.com>
2024-09-11 15:16:01 -0400

    fix input definition error

--- client/src/views/EDInputsReview.vue
+++ client/src/views/EDInputsReview.vue
@@ -986,7 +986,18 @@ export default {
             let data = await dataRequest("/api/experiment/core/" + this.experimentID, "GET");
             console.log(data);
             this.coreUsage = data.map(e => e.available);
+            console.log(data)
             let coreModelData = data.map(({ experiment_core_id, available, core, ...rest }) => {
+                console.log({
+                    experiment_core_id: experiment_core_id,
+                    available: available,
+                    core_model_id: core.core_model.core_model_id,
+                    core_number: core.core_model.core_number,
+                    model_number: core.core_model.model_number,
+                    status: core.core_model.status,
+                    created: core.core_model.created,
+                    last_modified: core.core_model.last_modified
+                })
                 return {
                     experiment_core_id: experiment_core_id,
                     available: available,

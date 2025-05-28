824ab375b76f84b83b5c2bb3ade9f241319729e3
Dustin Huntsberger <dustin.huntsberger@engusasecure.com>
2025-05-28 11:33:23 -0400

    update hours of operation admin page

--- client/src/views/AdminHOO.vue
+++ client/src/views/AdminHOO.vue
@@ -5,40 +5,40 @@
         <AdminSidebar />
         <div class="content">
             <h1>Hours of Operation Management</h1>
-            <div>
-                <h2>Monday</h2>
+            <div v-for="day in this.days">
+                <h2>{{ day[0].toUpperCase() + day.shift() }}</h2>
                 <table class="grid-less">
                     <tr>
-                        <th>Opens Monday?</th>
+                        <th>Opens {{ day[0].toUpperCase() + day.shift() }}?</th>
                         <td>
                             <label class="switch">
-                                <input name="mon-closes" id="mon-closes" type="checkbox"
-                                    :checked="this.closingData.monday.opens"
-                                    @change="e => this.closingData.monday.opens = e.target.checked" />
+                                <input :name="day+'-closes'" :id="day+'-closes'" type="checkbox"
+                                    :checked="this.closingData[day].opens"
+                                    @change="e => this.closingData[day].opens = e.target.checked" />
                                 <span class="slider round"></span>
                             </label>
                         </td>
                     </tr>
                     <tr>
-                        <th>Closes Monday?</th>
+                        <th>Closes {{ day[0].toUpperCase() + day.shift() }}?</th>
                         <td>
                             <label class="switch">
-                                <input name="mon-closes" id="mon-closes" type="checkbox"
-                                    :checked="this.closingData.monday.closes"
-                                    @change="e => this.closingData.monday.closes = e.target.checked" />
+                                <input :name="day+'-closes'" :id="day+'-closes'" type="checkbox"
+                                    :checked="this.closingData[day].closes"
+                                    @change="e => this.closingData[day].closes = e.target.checked" />
                                 <span class="slider round"></span>
                             </label>
                         </td>
                     </tr>
-                    <tr v-if="this.closingData.monday.opens">
-                        <th><label for="mon-start">Start:</label></th>
-                        <td><input name="mon-start" id="mon-start" type="time" :value="this.closingData.monday.starts"
-                                step="3600" @change="timeChange($event, 'monday', 'start')" /></td>
+                    <tr v-if="this.closingData[day].opens">
+                        <th><label :for="day+'-start'">Start:</label></th>
+                        <td><input :name="day+'-start'" :id="day+'-start'" type="time" :value="this.closingData[day].starts"
+                                step="3600" @change="timeChange($event, day, 'start')" /></td>
                     </tr>
-                    <tr v-if="this.closingData.monday.opens && this.closingData.monday.closes">
-                        <th><label for="mon-start">End:</label></th>
-                        <td><input name="mon-start" id="mon-start" type="time" :value="this.closingData.monday.ends"
-                                step="3600" @change="timeChange($event, 'monday', 'end')" /></td>
+                    <tr v-if="this.closingData[day].opens && this.closingData[day].closes">
+                        <th><label :for="day+'-end'">End:</label></th>
+                        <td><input :name="day+'-end'" :id="day+'-end'" type="time" :value="this.closingData[day].ends"
+                                step="3600" @change="timeChange($event, day, 'end')" /></td>
                     </tr>
                 </table>
             </div>
@@ -102,93 +102,6 @@ export default {
                     this.closingData[this.days[i]].ends = null;
                 }
             }
-            // // monday
-            // let mondayEntry = data.find(e => e.day_num == 0);
-            // if (mondayEntry.start_time !== null) {
-            //     this.closingData.monday.opens = true;
-            //     this.closingData.monday.starts = mondayEntry.start_time
-            // } else {
-            //     this.closingData.monday.opens = false;
-            //     this.closingData.monday.starts = null;
-            // }
-            // if (mondayEntry.end_time !== null) {
-            //     this.closingData.monday.closes = true;
-            //     this.closingData.monday.ends = mondayEntry.end_time;
-            // } else {
-            //     this.closingData.monday.closes = false;
-            //     this.closingData.monday.ends = null;
-            // }
-            // // tuesday
-            // let tuesdayEntry = data.find(e => e.day_num == 1);
-            // if (tuesdayEntry.start_time !== null) {
-            //     this.closingData.tuesday.opens = true;
-            //     this.closingData.tuesday.starts = tuesdayEntry.start_time;
-            // } else {
-            //     this.closingData.tuesday.opens = false;
-            //     this.closingData.tuesday.starts = null;
-            // }
-            // if (tuesdayEntry.end_time !== null) {
-            //     this.closingData.tuesday.closes = true;
-            //     this.closingData.tuesday.ends = tuesdayEntry.end_time;
-            // } else {
-            //     this.closingData.tuesday.closes = false;
-            //     this.closingData.tuesday.ends = null;
-            // }
-            // // wednesday
-            // if (data.find(e => e.day_num == 2).start_time !== null) {
-            //     this.closingData.wednesday.opens = true;
-            // } else {
-            //     this.closingData.wednesday.opens = false;
-            // }
-            // if (data.find(e => e.day_num == 2).end_time !== null) {
-            //     this.closingData.wednesday.closes = true;
-            // } else {
-            //     this.closingData.wednesday.closes = false;
-            // }
-            // // thursday
-            // if (data.find(e => e.day_num == 3).start_time !== null) {
-            //     this.closingData.thursday.opens = true;
-            // } else {
-            //     this.closingData.thursday.opens = false;
-            // }
-            // if (data.find(e => e.day_num == 3).end_time !== null) {
-            //     this.closingData.thursday.closes = true;
-            // } else {
-            //     this.closingData.thursday.closes = false;
-            // }
-            // //friday
-            // if (data.find(e => e.day_num == 4).start_time !== null) {
-            //     this.closingData.friday.opens = true;
-            // } else {
-            //     this.closingData.friday.opens = false;
-            // }
-            // if (data.find(e => e.day_num == 4).end_time !== null) {
-            //     this.closingData.friday.closes = true;
-            // } else {
-            //     this.closingData.friday.closes = false;
-            // }
-            // //saturday
-            // if (data.find(e => e.day_num == 5).start_time !== null) {
-            //     this.closingData.saturday.opens = true;
-            // } else {
-            //     this.closingData.saturday.opens = false;
-            // }
-            // if (data.find(e => e.day_num == 5).end_time !== null) {
-            //     this.closingData.saturday.closes = true;
-            // } else {
-            //     this.closingData.saturday.closes = false;
-            // }
-            // //sunday
-            // if (data.find(e => e.day_num == 6).start_time !== null) {
-            //     this.closingData.sunday.opens = true;
-            // } else {
-            //     this.closingData.sunday.opens = false;
-            // }
-            // if (data.find(e => e.day_num == 6).end_time !== null) {
-            //     this.closingData.sunday.closes = true;
-            // } else {
-            //     this.closingData.sunday.closes = false;
-            // }
             console.log(this.closingData);
         },
         timeChange(e, day, type) {

d8199e23c1ce6228a8a7dd799a639b6006a7bd10
Dustin Huntsberger <dustin.huntsberger@engusasecure.com>
2025-06-05 03:00:28 -0400

    continue to add continuous mode functionality

--- server/controllers/api/experiment/processTimeTypeRoutes.js
+++ server/controllers/api/experiment/processTimeTypeRoutes.js
@@ -27,7 +27,9 @@ router.get('/:id', (req, res) => {
 router.put('/', async (req, res) => {
     let promises = []
     try {
+        console.log("hey");
         console.log(req.body[0]);
+        console.log("look");
         if (req.body[0].iteration_number == 1) {
             req.body.forEach(entry => {
                 promises.push(ExperimentTimeType.update(entry, {

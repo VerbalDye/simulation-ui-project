dae50043ef5921fffd64bb280cdb1c4270f696e2
Dustin Huntsberger <dustin.huntsberger@engusasecure.com>
2025-06-05 05:08:54 -0400

    add core admin functionality

--- client/src/views/AdminCore.vue
+++ client/src/views/AdminCore.vue
@@ -103,7 +103,14 @@ export default {
             this.shownTableData = this.filteredTableData.slice(this.page, this.page + this.pageCount - 1);
         },
         coreSearchChange(e) {
-            this.filteredTableData = JSON.parse(JSON.stringify(this.tableData.filter(f => f.core_number.toString().includes(e.target.value) || f.model_number.toString().includes(e.target.value) || f.core_oven_drawer_position.includes(e.target.value) || f.core_oven_number.toString().includes(e.target.value) || f.soak_temperature_f.toString().includes(e.target.value) || f.time_minutes.toString().includes(e.target.value))));
+            this.filteredTableData = JSON.parse(JSON.stringify(this.tableData.filter(f => {
+                let searchString = f.core_number.toString() + f.model_number + f.core_oven_drawer_position + f.core_oven_number + f.soak_temperature_f + f.time_minutes;
+                if (searchString.includes(e.target.value)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            })));
             this.changePage('reload');
         },
         saveChanges() {

4324dcb013a3ef5bcc8e37b3d8fc76265e87e9b0
Dustin Huntsberger <dustin.huntsberger@engusasecure.com>
2025-06-05 05:31:52 -0400

    add core admin functionality

--- client/src/views/AdminCore.vue
+++ client/src/views/AdminCore.vue
@@ -6,8 +6,45 @@
         <div class="content">
             <h1>Core Management</h1>
             <div>
+                <div>
+                    <h2>Add Core</h2>
+                    <table class="full-table">
+                        <tr>
+                            <th>Core Number</th>
+                            <th>Model Number</th>
+                            <th>Oven Drawer Position</th>
+                            <th>Oven Number</th>
+                            <th>Oven Temperature (f)</th>
+                            <th>Soak Time (minutes)</th>
+                            <th>Status</th>
+                        </tr>
+                        <tr>
+                            <td>{{ entry.core_number }}</td>
+                            <td>{{ entry.model_number }}</td>
+                            <td><select>
+                                    <option value="1F" :selected="entry.core_oven_drawer_position == '1F'">1F</option>
+                                    <option value="2F" :selected="entry.core_oven_drawer_position == '2F'">2F</option>
+                                </select></td>
+                            <td>{{ entry.core_oven_number }}</td>
+                            <td><input type="number" :value="entry.soak_temperature_f" /></td>
+                            <td><input type="number" :value="entry.time_minutes" /></td>
+                            <td><select>
+                                    <option value="R&D" :selected="entry.status == 'R&D'">R&D</option>
+                                    <option value="APPROVED" :selected="entry.status == 'APPROVED'">APPROVED</option>
+                                    <option value="SCRAP" :selected="entry.status == 'SCRAP'">SCRAP</option>
+                                    <option value="QUARANTINE" :selected="entry.status == 'QUARANTINE'">QUARANTINE
+                                    </option>
+                                    <option value="REPAIR" :selected="entry.status == 'REPAIR'">REPAIR</option>
+                                    <option value="CANCELLED" :selected="entry.status == 'CANCELLED'">CANCELLED</option>
+                                    <option value="DEVELOP" :selected="entry.status == 'DEVELOP'">DEVELOP</option>
+                                    <option value="SOLD" :selected="entry.status == 'SOLD'">SOLD</option>
+                                </select></td>
+                        </tr>
+                    </table>
+                    <button>Add Core</button>
+                </div>
                 <label for="core-search">Search: </label>
-                <input name="core-search" id="core-search" type="text" @change="coreSearchChange($event)"/>
+                <input name="core-search" id="core-search" type="text" @change="coreSearchChange($event)" />
                 <table class="full-table">
                     <tr>
                         <th>Core Number</th>
@@ -16,29 +53,41 @@
                         <th>Oven Number</th>
                         <th>Oven Temperature (f)</th>
                         <th>Soak Time (minutes)</th>
+                        <th>Status</th>
                         <th>Delete?</th>
                     </tr>
                     <tr v-for="entry in shownTableData">
                         <td>{{ entry.core_number }}</td>
                         <td>{{ entry.model_number }}</td>
                         <td><select>
-                            <option value="1F" :selected="entry.core_oven_drawer_position == '1F'">1F</option>
-                            <option value="2F" :selected="entry.core_oven_drawer_position == '2F'">2F</option>
-                        </select></td>
+                                <option value="1F" :selected="entry.core_oven_drawer_position == '1F'">1F</option>
+                                <option value="2F" :selected="entry.core_oven_drawer_position == '2F'">2F</option>
+                            </select></td>
                         <td>{{ entry.core_oven_number }}</td>
-                        <td><input type="number" :value="entry.soak_temperature_f"/></td>
-                        <td><input type="number" :value="entry.time_minutes"/></td>
+                        <td><input type="number" :value="entry.soak_temperature_f" /></td>
+                        <td><input type="number" :value="entry.time_minutes" /></td>
+                        <td><select>
+                                <option value="R&D" :selected="entry.status == 'R&D'">R&D</option>
+                                <option value="APPROVED" :selected="entry.status == 'APPROVED'">APPROVED</option>
+                                <option value="SCRAP" :selected="entry.status == 'SCRAP'">SCRAP</option>
+                                <option value="QUARANTINE" :selected="entry.status == 'QUARANTINE'">QUARANTINE</option>
+                                <option value="REPAIR" :selected="entry.status == 'REPAIR'">REPAIR</option>
+                                <option value="CANCELLED" :selected="entry.status == 'CANCELLED'">CANCELLED</option>
+                                <option value="DEVELOP" :selected="entry.status == 'DEVELOP'">DEVELOP</option>
+                                <option value="SOLD" :selected="entry.status == 'SOLD'">SOLD</option>
+                            </select></td>
                         <td><button @click="deleteRow(entry.core_number)">Delete</button></td>
                     </tr>
                 </table>
                 <div>
-                    Showing {{ page + 1 }} - {{ page + pageCount > filteredTableData.length ? filteredTableData.length : page + pageCount }} of {{ filteredTableData.length }}
+                    Showing {{ page + 1 }} - {{ page + pageCount > filteredTableData.length ? filteredTableData.length :
+                        page + pageCount }} of {{ filteredTableData.length }}
                 </div>
                 <button @click="changePage('skip-back')"><i class="bi bi-chevron-double-left"></i></button>
                 <button @click="changePage('back')"><i class="bi bi-chevron-left"></i></button>
                 <button @click="changePage('forward')"><i class="bi bi-chevron-right"></i></button>
                 <button @click="changePage('skip-forward')"><i class="bi bi-chevron-double-right"></i></button>
-                <br/>
+                <br />
                 <button @click="saveChanges" class="space">Save</button>
             </div>
         </div>
@@ -79,6 +128,7 @@ export default {
                     core_oven_number: entry.core.core_soak_times[0].core_oven_number,
                     soak_temperature_f: entry.core.core_soak_times[0].soak_temperature_f,
                     time_minutes: entry.core.core_soak_times[0].time_minutes,
+                    status: entry.status
                 })
             })
             console.log(this.tableData);
@@ -86,16 +136,16 @@ export default {
             this.shownTableData = this.filteredTableData.slice(0, this.pageCount - 1);
             this.coreModelData = data;
         },
-        changePage(type){
+        changePage(type) {
             if (type == "skip-back") {
                 this.page = 0
             } else if (type == "back") {
                 if (this.page - this.pageCount >= 0) {
-                this.page = this.page - this.pageCount
+                    this.page = this.page - this.pageCount
                 }
             } else if (type == "forward") {
                 if (this.page + this.pageCount <= this.filteredTableData.length) {
-                this.page = this.page + this.pageCount
+                    this.page = this.page + this.pageCount
                 }
             } else if (type == "skip-forward") {
                 this.page = Math.floor(this.filteredTableData.length / this.pageCount) * this.pageCount
@@ -104,7 +154,7 @@ export default {
         },
         coreSearchChange(e) {
             this.filteredTableData = JSON.parse(JSON.stringify(this.tableData.filter(f => {
-                let searchString = f.core_number.toString() + f.model_number + f.core_oven_drawer_position + f.core_oven_number + f.soak_temperature_f + f.time_minutes;
+                let searchString = f.core_number.toString() + f.model_number + f.core_oven_drawer_position + f.core_oven_number + f.soak_temperature_f + f.time_minutes + f.status;
                 if (searchString.includes(e.target.value)) {
                     return true;
                 } else {
@@ -116,6 +166,7 @@ export default {
         deleteRow(core_number) {
             this.tableData = this.tableData.filter(e => e.core_number !== core_number);
             this.filteredTableData = this.filteredTableData.filter(e => e.core_number !== core_number);
+            this.changePage('reload');
         },
         saveChanges() {
 

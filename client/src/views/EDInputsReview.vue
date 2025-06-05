69574ea8e767ec562e72cc8ae1c5b08fe22cf72a
Dustin Huntsberger <dustin.huntsberger@engusasecure.com>
2025-06-04 20:47:16 -0400

    continue to add continuous mode functionality

--- client/src/views/EDInputsReview.vue
+++ client/src/views/EDInputsReview.vue
@@ -390,7 +390,7 @@
                                                                 v-if="this.processTimeSettings.distributionType !== 'Normal'">
                                                                 <th><i class="bi bi-dash-lg"></i> Min (minutes)*</th>
                                                                 <td>
-                                                                    <input type="number" :value="this.processTimeSettings.continuousElements[this.selectedAssets[0].asset_id].value.min"
+                                                                    <input type="number" :value="this.processTimeSettings.continuousElements[this.selectedAssets[0].asset_id].values.min"
                                                                         class="small-number-input" />
                                                                 </td>
                                                             </tr>
@@ -398,7 +398,7 @@
                                                                 v-if="this.processTimeSettings.distributionType == 'Beta' || this.processTimeSettings.distributionType == 'Triangular' || this.processTimeSettings.distributionType == 'Uniform'">
                                                                 <th><i class="bi bi-plus-lg"></i> Max (minutes)*</th>
                                                                 <td>
-                                                                    <input type="number" :value="this.processTimeSettings.continuousElements[this.selectedAssets[0].asset_id].value.max"
+                                                                    <input type="number" :value="this.processTimeSettings.continuousElements[this.selectedAssets[0].asset_id].values.max"
                                                                         class="small-number-input" />
                                                                 </td>
                                                             </tr>
@@ -417,7 +417,7 @@
                                                                 </th>
                                                                 <td>
                                                                     <input type="number" class="small-number-input"
-                                                                        :value="this.processTimeSettings.continuousElements[this.selectedAssets[0].asset_id].value.param1">
+                                                                        :value="this.processTimeSettings.continuousElements[this.selectedAssets[0].asset_id].values.param1">
                                                                 </td>
                                                             </tr>
                                                             <tr
@@ -432,7 +432,7 @@
                                                                 </th>
                                                                 <td>
                                                                     <input type="number" class="small-number-input"
-                                                                        :value="this.processTimeSettings.continuousElements[this.selectedAssets[0].asset_id].value.param2">
+                                                                        :value="this.processTimeSettings.continuousElements[this.selectedAssets[0].asset_id].values.param2">
                                                                 </td>
                                                             </tr>
                                                         </table>

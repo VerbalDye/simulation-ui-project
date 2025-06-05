7045dc717e640a6b1af8cc4b81531ea9c30f2727
Dustin Huntsberger <dustin.huntsberger@engusasecure.com>
2025-06-05 13:16:51 -0400

    add job admin functionality

--- client/src/views/AdminJob.vue
+++ client/src/views/AdminJob.vue
@@ -5,6 +5,29 @@
         <AdminSidebar />
         <div class="content">
             <h1>Job Management</h1>
+            <div>
+                <button @click="downloadTemplate">Download Template</button>
+                <!-- <div v-if="this.backlogData" class="space">
+                    This experiment has Backlog Data. If you would like to overwrite, upload new backlog
+                    data
+                    below and click "Upload".
+                </div> -->
+                <h4>Start Date</h4>
+                <input type="date" :value="demandSettings.startDate" name="backlog-start-date-input" class="space"
+                    @input="e => demandSettings.startDate = e.target.value">
+                <div>
+                    <label for="backlog-input">Upload Backlog</label>
+                    <input type="file" name="backlog-input" id="backlog-input" class="space" accept=".csv">
+                </div>
+                <div class="flex-right">
+                    <button @click="uploadBacklog">Upload</button>
+                </div>
+                <div v-if="jobData && jobDropdownData" class="limit-width">
+                    <SmartTable :jsonData="jobData" :advancedSearchEnabled="false" :id="4"
+                        :excludedColumns="['job_location_id', 'job_core_id']" :dropdownData="jobDropdownData"
+                        @selection-change="handleJobSelectionChange" />
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -16,6 +39,7 @@ import SmartTable from '@/components/SmartTable.vue';
 import AdminSidebar from '@/components/AdminSidebar.vue';
 import titleMixin from '../mixins/titleMixin';
 import dataRequest from '@/utils/dataRequest';
+import SmartTable from '@/components/SmartTable.vue';
 export default {
     data() {
         return {

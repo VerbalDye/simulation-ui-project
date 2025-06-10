855036192b3a0bd5525372482efdd975b7b3438f
Dustin Huntsberger <dustin.huntsberger@engusasecure.com>
2025-06-09 23:46:21 -0400

    fix admin job

--- client/src/views/AdminJob.vue
+++ client/src/views/AdminJob.vue
@@ -3,10 +3,30 @@
     <div class="site-container">
         <Sidebar />
         <AdminSidebar />
-        <div class="content">
+        <div>
             <h1>Job Management</h1>
             <div>
-                
+                <!-- <button @click="downloadTemplate">Download Template</button> -->
+                <!-- <div v-if="this.backlogData" class="space">
+                    This experiment has Backlog Data. If you would like to overwrite, upload new backlog
+                    data
+                    below and click "Upload".
+                </div> -->
+                <!-- <h4>Start Date</h4>
+                <input type="date" :value="demandSettings.startDate" name="backlog-start-date-input" class="space"
+                    @input="e => demandSettings.startDate = e.target.value">
+                <div>
+                    <label for="backlog-input">Upload Backlog</label>
+                    <input type="file" name="backlog-input" id="backlog-input" class="space" accept=".csv">
+                </div>
+                <div class="flex-right">
+                    <button @click="uploadBacklog">Upload</button>
+                </div> -->
+                <!-- <div v-if="jobData && jobDropdownData" class="limit-width">
+                    <SmartTable :jsonData="jobData" :advancedSearchEnabled="false" :id="4"
+                        :excludedColumns="['job_location_id', 'job_core_id']" :dropdownData="jobDropdownData"
+                        @selection-change="handleJobSelectionChange" />
+                </div> -->
             </div>
         </div>
     </div>

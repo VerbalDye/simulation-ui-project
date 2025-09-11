1725cac24e1a2bb795a48f48312a7d4f9b6c1bd8
Dustin Huntsberger <dustin.huntsberger@engusasecure.com>
2025-09-11 09:52:13 -0400

    add priority mockup

--- server/models/Shift.js
+++ server/models/Shift.js
@@ -32,7 +32,7 @@ Shift.init(
             allowNull: false,
             defaultValue: DataTypes.NOW
         },
-        last_modified: {
+        modified: {
             type: DataTypes.DATE,
             allowNull: false,
             defaultValue: DataTypes.NOW

3defb56496b063fcb4b1563c05b1e7914708ae8c
Dustin Huntsberger <dustin.huntsberger@engusasecure.com>
2025-10-01 07:16:22 -0400

    add anylogic animation

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,4 @@ import App from './App.vue'
 import router from './router'
 import LoadScript from 'vue-plugin-load-script';
 
-let app = createApp(App).use(router).mount('#app');
-app.use(LoadScript);
-
+createApp(App).use(router).use(LoadScript).mount('#app');

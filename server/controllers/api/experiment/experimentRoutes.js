5239fcaa71fd3c50ee10c42f210c36c2f5019825
Dustin Huntsberger <dustin.huntsberger@engusasecure.com>
2025-12-17 12:03:32 -0500

    add worker skills deselect button

--- server/controllers/api/experiment/experimentRoutes.js
+++ server/controllers/api/experiment/experimentRoutes.js
@@ -431,7 +431,7 @@ router.post('/from/:id', (req, res) => {
                 model: ExperimentShift
             }]
         })
-        console.log(dbExperimentShiftData[0].dataValues.experiment_shift);
+        console.log(dbExperimentShiftData);
         // let newShiftData = dbExperimentShiftData.dataValues.experiment_shift.dataValues.map(e => {
         //     return {
         //         experiment_id: e.experiment_id,

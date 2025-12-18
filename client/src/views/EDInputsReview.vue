702340a67b4b1e7ccfc5b9cd29a48bc4f881b4bc
Dustin Huntsberger <dustin.huntsberger@engusasecure.com>
2025-12-18 14:48:40 -0500

    add cutback as its own skill

--- client/src/views/EDInputsReview.vue
+++ client/src/views/EDInputsReview.vue
@@ -1583,7 +1583,8 @@ export default {
             console.log(this.formattedTaskSequenceData);
             this.selectedOperation = 2;
             this.cellNames = this.formattedTaskSequenceData.filter(e => e.type == 'cell').map(e => e.displayName);
-            this.cellNames.push("Cutback");
+            this.cellNames = this.cellNames.filter(e => e !== 'Incoming-Outside' || e !== 'Shipping/Delivery');
+            this.cellNames.splice(this.cellName.length-2, 0, "Cutback");
         },
         async getPriorityData() {
             let data = await dataRequest("/api/experiment/priority/" + this.experimentID, "GET");
